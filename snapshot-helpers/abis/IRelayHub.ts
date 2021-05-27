a7dd5694eb7b80f318d8e9fdbe0ee692d06d1cb7
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-05-27 19:26:43 -0400

    cleanup unused data

--- snapshot-helpers/abis/IRelayHub.ts
+++ snapshot-helpers/abis/IRelayHub.ts
@@ -276,17 +276,6 @@ const IRelayHub = [
         ],
         name: "Penalized",
         type: "event",
-    },
-    //NOTE: added this approval event abi because interface.parseLogs is un
-    {
-        anonymous:false,
-        inputs:
-            [{ indexed:true, internalType: "address", name:"owner",type:"address"},
-             {indexed:true,"internalType":"address","name":"spender","type":"address"},
-             {indexed:false,internalType:"uint256",name:"amount",type:"uint256"}
-            ],
-            name:"Approval",
-            type:"event"
     }
 ];
 
