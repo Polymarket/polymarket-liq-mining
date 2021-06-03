0855419b6ab8051556f14848a64ab3c6691cc284
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-06-03 16:28:09 -0400

    ban list

---
+++ snapshot-helpers/ban_list.ts
@@ -0,0 +1,9 @@
+
+/**
+ * A list of addresses currently excluded from the snapshot calculation
+ */
+export const EXCLUDED_ACCOUNTS = [
+    "0x40cf9693426945c02d964f87e29e29ff7b943bdd", 
+    "0x57ea53b3cf624d1030b2d5f62ca93f249adc95ba", 
+    "0xfae93a6c3b825861c0a4b781f0e0c7bcddf2f9f9"
+];
