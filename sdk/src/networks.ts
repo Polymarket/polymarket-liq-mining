2f31506fff45a1eeccfd261528e594628a2bd4db
Matt <mttwlkr@gmail.com>
2022-02-18 12:34:33 -0700

    Add polygon to contracts

--- sdk/src/networks.ts
+++ sdk/src/networks.ts
@@ -30,6 +30,21 @@ export const MUMBAI_CONTRACTS = {
   },
 };
 
+export const POLYGON_CONTRACTS = {
+  usdc: {
+    distributor: ADDRESS_ZERO,
+    erc20: ADDRESS_ZERO,
+  },
+  uma: {
+    distributor: ADDRESS_ZERO,
+    erc20: ADDRESS_ZERO,
+  },
+  matic: {
+    distributor: ADDRESS_ZERO,
+    erc20: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+  },
+};
+
 export const LOCAL_CONTRACTS = {
   usdc: {
     distributor: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
@@ -49,6 +64,8 @@ export const getContracts = (network: number) => {
   switch (network) {
     case 1:
       return MAINNET_CONTRACTS;
+    case 137:
+      return POLYGON_CONTRACTS;
     case 31337:
       return LOCAL_CONTRACTS;
     case 80001:
