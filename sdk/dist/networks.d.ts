bc8bc76eefa672c98a8f7d35b3048a570e44ada9
Matt <mttwlkr@gmail.com>
2022-02-18 14:44:33 -0700

    Add build info

--- sdk/dist/networks.d.ts
+++ sdk/dist/networks.d.ts
@@ -1,11 +1,57 @@
-import { Token } from './types';
-declare type RegistryContracts = {
-    distributor: string;
-    [Token.Uma]: string;
-    [Token.Matic]: string;
+export declare const MAINNET_CONTRACTS: {
+    usdc: {
+        distributor: string;
+        erc20: string;
+    };
+    uma: {
+        distributor: string;
+        erc20: string;
+    };
+    matic: {
+        distributor: string;
+        erc20: string;
+    };
 };
-export declare const MAINNET_CONTRACTS: RegistryContracts;
-export declare const MUMBAI_CONTRACTS: RegistryContracts;
-export declare const getContracts: (network: number) => RegistryContracts;
-export {};
+export declare const MUMBAI_CONTRACTS: {
+    usdc: {
+        distributor: string;
+        erc20: string;
+    };
+    uma: {
+        distributor: string;
+        erc20: string;
+    };
+    matic: {
+        distributor: string;
+        erc20: string;
+    };
+};
+export declare const LOCAL_CONTRACTS: {
+    usdc: {
+        distributor: string;
+        erc20: string;
+    };
+    uma: {
+        distributor: string;
+        erc20: string;
+    };
+    matic: {
+        distributor: string;
+        erc20: string;
+    };
+};
+export declare const getContracts: (network: number) => {
+    usdc: {
+        distributor: string;
+        erc20: string;
+    };
+    uma: {
+        distributor: string;
+        erc20: string;
+    };
+    matic: {
+        distributor: string;
+        erc20: string;
+    };
+};
 //# sourceMappingURL=networks.d.ts.map
--- sdk/dist/networks.js
+++ sdk/dist/networks.js
@@ -1,24 +1,55 @@
 "use strict";
-var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getContracts = exports.MUMBAI_CONTRACTS = exports.MAINNET_CONTRACTS = void 0;
-var types_1 = require("./types");
-exports.MAINNET_CONTRACTS = (_a = {
-        distributor: ""
+exports.getContracts = exports.LOCAL_CONTRACTS = exports.MUMBAI_CONTRACTS = exports.MAINNET_CONTRACTS = void 0;
+var ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+exports.MAINNET_CONTRACTS = {
+    usdc: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
     },
-    _a[types_1.Token.Uma] = "",
-    _a[types_1.Token.Matic] = "",
-    _a);
-exports.MUMBAI_CONTRACTS = (_b = {
-        distributor: ""
+    uma: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
     },
-    _b[types_1.Token.Uma] = "",
-    _b[types_1.Token.Matic] = "",
-    _b);
+    matic: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
+    },
+};
+exports.MUMBAI_CONTRACTS = {
+    usdc: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
+    },
+    uma: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
+    },
+    matic: {
+        distributor: ADDRESS_ZERO,
+        erc20: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+    },
+};
+exports.LOCAL_CONTRACTS = {
+    usdc: {
+        distributor: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        erc20: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    },
+    uma: {
+        distributor: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+        erc20: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+    },
+    matic: {
+        distributor: ADDRESS_ZERO,
+        erc20: ADDRESS_ZERO,
+    },
+};
 var getContracts = function (network) {
     switch (network) {
         case 1:
             return exports.MAINNET_CONTRACTS;
+        case 31337:
+            return exports.LOCAL_CONTRACTS;
         case 80001:
             return exports.MUMBAI_CONTRACTS;
         default:
