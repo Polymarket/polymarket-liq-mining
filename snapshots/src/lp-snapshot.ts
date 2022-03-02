a7c345fba106020f7097e194ad6649cc421a6bd2
Matt <mttwlkr@gmail.com>
2022-03-02 11:46:24 -0700

    Add event start, event percent, tests

--- snapshots/src/lp-snapshot.ts
+++ snapshots/src/lp-snapshot.ts
@@ -6,14 +6,18 @@ import {
     getCurrentBlockNumber,
 } from "./block_numbers";
 import {
+    calculateSamplesPerEvent,
     calculateTokensPerSample,
     lowerCaseMarketMakers,
     LpMarketInfo,
     updateTokensPerBlockReward,
+	validateEventStartBlock,
 } from "./lp-helpers";
 import { getStartAndEndBlock } from "./lp-helpers";
 import { MapOfCount, ReturnSnapshot, ReturnType } from "./interfaces";
 import { addEoaToUserPayoutMap } from "./helpers";
+
+const NUMBER_OF_SAMPLES_PER_MARKET = 150;
 
 /**
  * Generate a lp weighted token snapshot
@@ -22,132 +26,158 @@ import { addEoaToUserPayoutMap } from "./helpers";
  * @returns
  */
 export async function generateLpSnapshot(
-  returnType: ReturnType,
-  startTimestamp: number,
-  endTimestamp: number,
-  marketMakers: LpMarketInfo[],
-  blocksPerSample: number
+    returnType: ReturnType,
+    startTimestamp: number,
+    endTimestamp: number,
+    marketMakers: LpMarketInfo[],
+    blocksPerSample: number,
 ): Promise<ReturnSnapshot[] | MapOfCount> {
-  console.log(`Generating lp snapshot with timestamp: ${endTimestamp}`);
+    console.log(`Generating lp snapshot with timestamp: ${endTimestamp}`);
 
-  let userTokensPerEpoch: { [proxyWallet: string]: number } = {};
-  const markets = lowerCaseMarketMakers(marketMakers);
+    let userTokensPerEpoch: { [proxyWallet: string]: number } = {};
+    const markets = lowerCaseMarketMakers(marketMakers);
 
-  let epochEndBlock = await convertTimestampToBlockNumber(endTimestamp);
-  while (!epochEndBlock) {
-    console.log("epochEndBlock was not found. trying again!");
-    epochEndBlock = await convertTimestampToBlockNumber(endTimestamp);
-  }
-
-  let epochStartBlock = await convertTimestampToBlockNumber(startTimestamp);
-  while (!epochStartBlock) {
-    console.log("epochStartBlock was not found. trying again!");
-    epochStartBlock = await convertTimestampToBlockNumber(startTimestamp);
-  }
-
-  for (const market of markets) {
-    const { marketMaker } = market;
-    const marketStartBlock = await getStartBlock(marketMaker);
-    const marketEndBlock = await getEndBlock(marketMaker);
-
-    let rewardMarketEndBlock = null;
-
-    if (market.rewardMarketEndDate) {
-      console.log("reward market end date exists, getting block!");
-      while (!rewardMarketEndBlock) {
-        console.log("reward market end block was not found. trying again!");
-        rewardMarketEndBlock = await convertTimestampToBlockNumber(
-          market.rewardMarketEndDate
-        );
-      }
+    let epochEndBlock = await convertTimestampToBlockNumber(endTimestamp);
+    while (!epochEndBlock) {
+        console.log("epochEndBlock was not found. trying again!");
+        epochEndBlock = await convertTimestampToBlockNumber(endTimestamp);
     }
 
-    let rewardMarketStartBlock = null;
-    if (market.rewardMarketStartDate) {
-      console.log("reward market start date exists, getting block!");
-      while (!rewardMarketStartBlock) {
-        console.log("reward market end block was not found. trying again!");
-        rewardMarketStartBlock = await convertTimestampToBlockNumber(
-          market.rewardMarketStartDate
-        );
-      }
+    let epochStartBlock = await convertTimestampToBlockNumber(startTimestamp);
+    while (!epochStartBlock) {
+        console.log("epochStartBlock was not found. trying again!");
+        epochStartBlock = await convertTimestampToBlockNumber(startTimestamp);
     }
 
-    const { startBlock, endBlock: eb } = getStartAndEndBlock({
-      epochStartBlock,
-      epochEndBlock,
-      marketStartBlock,
-      marketEndBlock,
-      rewardMarketEndBlock,
-      rewardMarketStartBlock,
-    });
+    for (const market of markets) {
+        const { marketMaker } = market;
+        const marketStartBlock = await getStartBlock(marketMaker);
+        const marketEndBlock = await getEndBlock(marketMaker);
 
-    const currentBlock = await getCurrentBlockNumber();
-    // if epoch has not ended and market has not resolved, get current block
-    const endBlock = !eb ? currentBlock : eb;
-    console.log({
-      epochStartBlock,
-      marketStartBlock,
-      rewardMarketStartBlock,
-      rewardMarketEndBlock,
-      marketEndBlock,
-      epochEndBlock,
-      currentBlock,
-      startBlockBeingUsed: startBlock,
-      endBlockBeingUsed: endBlock,
-    });
+        let rewardMarketEndBlock = null;
 
-    // roughly, if our systems can handle 168 samples per epoch(24 samples per day * 7 day epoch)
-    // then we should take reward_market_start & reward_market_end diff and divide by 168
+        if (market.rewardMarketEndDate) {
+            console.log("reward market end date exists, getting block!");
+            while (!rewardMarketEndBlock) {
+                console.log(
+                    "reward market end block was not found. trying again!",
+                );
+                rewardMarketEndBlock = await convertTimestampToBlockNumber(
+                    market.rewardMarketEndDate,
+                );
+            }
+        }
 
-    if (rewardMarketStartBlock && rewardMarketEndBlock) {
-      const diff = rewardMarketEndBlock - rewardMarketStartBlock;
-      const samples = Math.floor(diff / 168);
-      console.log( `Reward market start and end block exist. Custom sample size: ${samples}`);
-      blocksPerSample = samples;
+        let rewardMarketStartBlock = null;
+        if (market.rewardMarketStartDate) {
+            console.log("reward market start date exists, getting block!");
+            while (!rewardMarketStartBlock) {
+                console.log(
+                    "reward market end block was not found. trying again!",
+                );
+                rewardMarketStartBlock = await convertTimestampToBlockNumber(
+                    market.rewardMarketStartDate,
+                );
+            }
+        }
+
+        let eventStartBlock = null;
+        if (market.eventStartDate) {
+            console.log("event start date exists, getting block!");
+            while (!eventStartBlock) {
+                console.log("event start block was not found. trying again!");
+                eventStartBlock = await convertTimestampToBlockNumber(
+                    market.eventStartDate,
+                );
+            }
+        }
+
+        const { startBlock, endBlock: eb } = getStartAndEndBlock({
+            epochStartBlock,
+            epochEndBlock,
+            marketStartBlock,
+            marketEndBlock,
+            rewardMarketEndBlock,
+            rewardMarketStartBlock,
+        });
+
+        const currentBlock = await getCurrentBlockNumber();
+        // if epoch has not ended and market has not resolved, get current block
+        const endBlock = !eb ? currentBlock : eb;
+
+        console.log({
+            epochStartBlock,
+            marketStartBlock,
+            rewardMarketStartBlock,
+            rewardMarketEndBlock,
+            marketEndBlock,
+            epochEndBlock,
+            currentBlock,
+            startBlockBeingUsed: startBlock,
+            eventStartBlock,
+            endBlockBeingUsed: endBlock,
+        });
+
+        // roughly, if our systems can handle ~150 samples per epoch 
+        // then we should take reward_market_start & reward_market_end diff and divide by 150
+        if (rewardMarketStartBlock && rewardMarketEndBlock) {
+            blocksPerSample = calculateSamplesPerEvent(
+                rewardMarketStartBlock,
+                rewardMarketEndBlock,
+                NUMBER_OF_SAMPLES_PER_MARKET,
+            );
+            console.log(
+                `Reward market start and end block exist. Custom sample size: ${blocksPerSample}`,
+            );
+
+			if (eventStartBlock) {
+				validateEventStartBlock(rewardMarketStartBlock, eventStartBlock, rewardMarketEndBlock)
+			}
+        }
+
+        if (startBlock !== null && endBlock > startBlock) {
+            const samples: number[] = [];
+            for (
+                let block = startBlock;
+                block <= endBlock;
+                block += blocksPerSample
+            ) {
+                samples.push(block);
+            }
+
+            console.log(`Using ${market.howToCalculate} calculation`);
+
+            const tokensPerSample = calculateTokensPerSample(
+                market,
+                samples.length,
+                blocksPerSample,
+            );
+
+            console.log(`Using ${tokensPerSample} tokens per sample`);
+            console.log(
+                `Using ${tokensPerSample / blocksPerSample} tokens per block`,
+            );
+
+            console.log(
+                `Diff between now and endBlock is ${
+                    currentBlock - endBlock
+                } blocks. (43,200 = 1 day; 1,800 = 1 hour; 30 = 1 minute)`,
+            );
+
+            // get liquidity state across many blocks for a market
+            const liquidityAcrossBlocks =
+                await calculateValOfLpPositionsAcrossBlocks(
+                    marketMaker,
+                    samples,
+                );
+
+            userTokensPerEpoch = updateTokensPerBlockReward(
+                userTokensPerEpoch,
+                liquidityAcrossBlocks,
+                tokensPerSample,
+            );
+        }
+
+        return addEoaToUserPayoutMap(userTokensPerEpoch);
     }
-
-    //Ensure that the market occured within the blocks being checked
-    if (startBlock !== null && endBlock > startBlock) {
-      const samples: number[] = [];
-      for (
-        let block = startBlock;
-        block <= endBlock;
-        block += blocksPerSample
-      ) {
-        samples.push(block);
-      }
-
-      console.log(`Using ${market.howToCalculate} calculation`);
-
-      const tokensPerSample = calculateTokensPerSample(
-        market,
-        samples.length,
-        blocksPerSample
-      );
-      console.log(`Using ${tokensPerSample} tokens per sample`);
-      console.log(
-        `Using ${tokensPerSample / blocksPerSample} tokens per block`
-      );
-
-      console.log(
-        `Diff between now and endBlock is ${
-          currentBlock - endBlock
-        } blocks. (43,200 = 1 day; 1,800 = 1 hour; 30 = 1 minute)`
-      );
-
-      // get liquidity state across many blocks for a market
-      const liquidityAcrossBlocks = await calculateValOfLpPositionsAcrossBlocks(
-        marketMaker,
-        samples
-      );
-
-      userTokensPerEpoch = updateTokensPerBlockReward(
-        userTokensPerEpoch,
-        liquidityAcrossBlocks,
-        tokensPerSample
-      );
-    }
-
-    return addEoaToUserPayoutMap(userTokensPerEpoch);
 }
--- snapshots/tests/lp-helpers.spec.ts
+++ snapshots/tests/lp-helpers.spec.ts
@@ -1,215 +1,273 @@
 import {
-  calculateTokensPerSample,
-  IStartAndEndBlock,
-  LpCalculation,
-  updateTokensPerBlockReward,
+    calculateSamplesPerEvent,
+    calculateTokensPerSample,
+    IStartAndEndBlock,
+    LpCalculation,
+    updateTokensPerBlockReward,
 } from "../src/lp-helpers";
 
 import { expect } from "chai";
-import { getStartAndEndBlock, LpMarketInfo } from "../src/lp-helpers";
+import { getStartAndEndBlock, LpMarketInfo, validateEventStartBlock } from '../src/lp-helpers';
 
 describe("calculate samples correctly", () => {
-  let markets: LpMarketInfo[];
-  let blocksPerSample: number;
-  let numSamplesInMarket: number;
-  let alan, brian;
+    let markets: LpMarketInfo[];
+    let blocksPerSample: number;
+    let numSamplesInMarket: number;
+    let alan, brian;
 
-  beforeEach(() => {
-    blocksPerSample = 30;
-    numSamplesInMarket = 420;
-    alan = "0x00D3BB55A6259416BB8DeF0EB46818aD178326eB";
-    brian = "0x0322c202691B2f1Eb4c4aB01Ee0813796392a3f2";
-    markets = [
-      {
-        marketMaker: alan,
-        howToCalculate: LpCalculation.PerMarket,
-        amount: 1000,
-        rewardMarketEndDate: null,
-        rewardMarketStartDate: null,
-      },
-      {
-        marketMaker: brian,
-        howToCalculate: LpCalculation.PerMarket,
-        amount: 20000,
-        rewardMarketEndDate: 1645214223584,
-        rewardMarketStartDate: 1645209039584,
-      },
-    ];
-  });
+    beforeEach(() => {
+        blocksPerSample = 30;
+        numSamplesInMarket = 420;
+        alan = "0x00D3BB55A6259416BB8DeF0EB46818aD178326eB";
+        brian = "0x0322c202691B2f1Eb4c4aB01Ee0813796392a3f2";
+        markets = [
+            {
+                marketMaker: alan,
+                howToCalculate: LpCalculation.PerMarket,
+                amount: 1000,
+                rewardMarketEndDate: null,
+                rewardMarketStartDate: null,
+                eventStartDate: null,
+                preEventPercent: null,
+            },
+            {
+                marketMaker: brian,
+                howToCalculate: LpCalculation.PerMarket,
+                amount: 20000,
+                rewardMarketEndDate: 1645214223584,
+                rewardMarketStartDate: 1645209039584,
+                eventStartDate: null,
+                preEventPercent: null,
+            },
+        ];
+    });
 
-  it("should calculate tokens per sample based off tokens per market", async () => {
-    const tokensPerSample = calculateTokensPerSample(
-      markets[1],
-      numSamplesInMarket,
-      blocksPerSample
-    );
-    expect(tokensPerSample).to.eq(markets[1].amount / numSamplesInMarket);
-  });
+    it("should calculate tokens per sample based off tokens per market", async () => {
+        const tokensPerSample = calculateTokensPerSample(
+            markets[1],
+            numSamplesInMarket,
+            blocksPerSample,
+        );
+        expect(tokensPerSample).to.eq(markets[1].amount / numSamplesInMarket);
+    });
+});
+
+describe("calculate samples per event", () => {
+    it("should calculate samples per event", async () => {
+        const startBlock = 2100000;
+        const samplesPerMarket = 150;
+        const endBlock = 2120000;
+        const res = calculateSamplesPerEvent(
+            startBlock,
+            endBlock,
+            samplesPerMarket,
+        );
+        const expected = Math.floor((endBlock - startBlock) / samplesPerMarket);
+        expect(res).to.eq(expected);
+    });
+
+    it("should throw an error if endBlock is before startBlock", async () => {
+        const startBlock = 2120000;
+        const samplesPerMarket = 150;
+        const endBlock = 2100000;
+        expect(() =>
+            calculateSamplesPerEvent(startBlock, endBlock, samplesPerMarket),
+        ).to.throw();
+    });
+});
+
+describe("validate event blocks", () => {
+    it("should not throw an error if blocks are in order", async () => {
+        const startBlock = 2100000;
+        const eventStartBlock = 2110000;
+        const endBlock = 2120000;
+        expect(() =>
+            validateEventStartBlock(startBlock,eventStartBlock, endBlock)
+        ).to.not.throw();
+    });
+
+    it("should throw an error if event block is before start block", async () => {
+        const startBlock = 2100000;
+        const eventStartBlock = 2090000;
+        const endBlock = 2120000;
+        expect(() =>
+            validateEventStartBlock(startBlock,eventStartBlock, endBlock)
+        ).to.throw();
+    });
+
+    it("should throw an error if event block is after end block", async () => {
+        const startBlock = 2100000;
+        const eventStartBlock = 2110000;
+        const endBlock = 2105000;
+        expect(() =>
+            validateEventStartBlock(startBlock,eventStartBlock, endBlock)
+        ).to.throw();
+    });
 });
 
 describe("updateTokens on a PerBlockReward", () => {
-  let initialState, alan, brian, carrie, devon, earl;
+    let initialState, alan, brian, carrie, devon, earl;
 
-  beforeEach(() => {
-    alan = "0x00D3BB55A6259416BB8DeF0EB46818aD178326eB";
-    brian = "0x0322c202691B2f1Eb4c4aB01Ee0813796392a3f2";
-    carrie = "0x0364E487CCd5a61d3c83848a420846848aE08061";
-    devon = "0x03d47623592049d5B402694E205AB12318d53a91";
-    earl = "0x04004b2A058F38df685dB22496c36dc4598F3F07";
+    beforeEach(() => {
+        alan = "0x00D3BB55A6259416BB8DeF0EB46818aD178326eB";
+        brian = "0x0322c202691B2f1Eb4c4aB01Ee0813796392a3f2";
+        carrie = "0x0364E487CCd5a61d3c83848a420846848aE08061";
+        devon = "0x03d47623592049d5B402694E205AB12318d53a91";
+        earl = "0x04004b2A058F38df685dB22496c36dc4598F3F07";
 
-    initialState = {
-      [alan]: 0.0000361591034695245,
-      [brian]: 106.91813466183105,
-      [carrie]: 80.34668192769267,
-      [devon]: 186.31548306498135,
-      [earl]: 51023.12194929194422,
-    };
-  });
+        initialState = {
+            [alan]: 0.0000361591034695245,
+            [brian]: 106.91813466183105,
+            [carrie]: 80.34668192769267,
+            [devon]: 186.31548306498135,
+            [earl]: 51023.12194929194422,
+        };
+    });
 
-  it("should not erase existing state", async () => {
-    const liquidity = [{ [alan]: 69 }];
-    const newState = updateTokensPerBlockReward(initialState, liquidity, 1);
-    expect(newState[devon]).to.eq(initialState[devon]);
-  });
+    it("should not erase existing state", async () => {
+        const liquidity = [{ [alan]: 69 }];
+        const newState = updateTokensPerBlockReward(initialState, liquidity, 1);
+        expect(newState[devon]).to.eq(initialState[devon]);
+    });
 
-  it("should work with no initial state", async () => {
-    const newAmount = 100;
-    const liquidity = [{ [alan]: newAmount, [brian]: newAmount }];
-    const newState = updateTokensPerBlockReward({}, liquidity, newAmount);
-    expect(newState[alan]).to.eq(newAmount / 2);
-    expect(newState[brian]).to.eq(newAmount / 2);
-  });
+    it("should work with no initial state", async () => {
+        const newAmount = 100;
+        const liquidity = [{ [alan]: newAmount, [brian]: newAmount }];
+        const newState = updateTokensPerBlockReward({}, liquidity, newAmount);
+        expect(newState[alan]).to.eq(newAmount / 2);
+        expect(newState[brian]).to.eq(newAmount / 2);
+    });
 
-  it("should work correctly with multiple blocks", async () => {
-    const perBlockReward = 10;
-    const perBlockLiquidity = 300;
-    const aPercent = 0.75;
-    const bPercent = 0.2;
-    const ePercent = 0.05;
-    const numOfBlocks = 3;
-    const liquidity = [
-      {
-        [alan]: perBlockLiquidity * aPercent,
-        [brian]: perBlockLiquidity * bPercent,
-        [earl]: perBlockLiquidity * ePercent,
-      },
-      {
-        [alan]: perBlockLiquidity * aPercent,
-        [brian]: perBlockLiquidity * bPercent,
-        [earl]: perBlockLiquidity * ePercent,
-      },
-      {
-        [alan]: perBlockLiquidity * aPercent,
-        [brian]: perBlockLiquidity * bPercent,
-        [earl]: perBlockLiquidity * ePercent,
-      },
-    ];
-    const newState = updateTokensPerBlockReward(
-      initialState,
-      liquidity,
-      perBlockReward
-    );
-    expect(newState[alan]).to.eq(
-      initialState[alan] + perBlockReward * aPercent * numOfBlocks
-    );
-    expect(newState[brian]).to.eq(
-      initialState[brian] + perBlockReward * bPercent * numOfBlocks
-    );
-    expect(newState[earl]).to.eq(
-      initialState[earl] + perBlockReward * ePercent * numOfBlocks
-    );
-  });
+    it("should work correctly with multiple blocks", async () => {
+        const perBlockReward = 10;
+        const perBlockLiquidity = 300;
+        const aPercent = 0.75;
+        const bPercent = 0.2;
+        const ePercent = 0.05;
+        const numOfBlocks = 3;
+        const liquidity = [
+            {
+                [alan]: perBlockLiquidity * aPercent,
+                [brian]: perBlockLiquidity * bPercent,
+                [earl]: perBlockLiquidity * ePercent,
+            },
+            {
+                [alan]: perBlockLiquidity * aPercent,
+                [brian]: perBlockLiquidity * bPercent,
+                [earl]: perBlockLiquidity * ePercent,
+            },
+            {
+                [alan]: perBlockLiquidity * aPercent,
+                [brian]: perBlockLiquidity * bPercent,
+                [earl]: perBlockLiquidity * ePercent,
+            },
+        ];
+        const newState = updateTokensPerBlockReward(
+            initialState,
+            liquidity,
+            perBlockReward,
+        );
+        expect(newState[alan]).to.eq(
+            initialState[alan] + perBlockReward * aPercent * numOfBlocks,
+        );
+        expect(newState[brian]).to.eq(
+            initialState[brian] + perBlockReward * bPercent * numOfBlocks,
+        );
+        expect(newState[earl]).to.eq(
+            initialState[earl] + perBlockReward * ePercent * numOfBlocks,
+        );
+    });
 });
 
 const mockState = {
-  epochStartBlock: 21000000,
-  marketStartBlock: 21400000,
-  marketEndBlock: 21600000,
-  epochEndBlock: 21900000,
-  rewardMarketEndBlock: null,
-  rewardMarketStartBlock: null,
+    epochStartBlock: 21000000,
+    marketStartBlock: 21400000,
+    marketEndBlock: 21600000,
+    epochEndBlock: 21900000,
+    rewardMarketEndBlock: null,
+    rewardMarketStartBlock: null,
 };
 
 describe("calculate correct start and end blocks", () => {
-  let initialState: IStartAndEndBlock = mockState;
-  // 22291274
-  beforeEach(() => {
-    initialState = { ...mockState };
-  });
+    let initialState: IStartAndEndBlock = mockState;
+    // 22291274
+    beforeEach(() => {
+        initialState = { ...mockState };
+    });
 
-  // STARTS
-  it("if the epoch starts after the market, start block is epoch block", async () => {
-    initialState.epochStartBlock = 21500000;
-    const { startBlock } = getStartAndEndBlock(initialState);
-    expect(startBlock).to.eq(initialState.epochStartBlock);
-  });
+    // STARTS
+    it("if the epoch starts after the market, start block is epoch block", async () => {
+        initialState.epochStartBlock = 21500000;
+        const { startBlock } = getStartAndEndBlock(initialState);
+        expect(startBlock).to.eq(initialState.epochStartBlock);
+    });
 
-  it("if the epoch starts before the market, start block is market block", async () => {
-    const { startBlock } = getStartAndEndBlock(initialState);
-    expect(startBlock).to.eq(initialState.marketStartBlock);
-  });
+    it("if the epoch starts before the market, start block is market block", async () => {
+        const { startBlock } = getStartAndEndBlock(initialState);
+        expect(startBlock).to.eq(initialState.marketStartBlock);
+    });
 
-  it("if there is a reward start date, use it", async () => {
-    const rmsb = 21500000;
-    initialState.rewardMarketStartBlock = rmsb;
-    const { startBlock } = getStartAndEndBlock(initialState);
-    expect(startBlock).to.eq(rmsb);
-  });
+    it("if there is a reward start date, use it", async () => {
+        const rmsb = 21500000;
+        initialState.rewardMarketStartBlock = rmsb;
+        const { startBlock } = getStartAndEndBlock(initialState);
+        expect(startBlock).to.eq(rmsb);
+    });
 
-  // ERRORS
+    // ERRORS
 
-  it("if there are no start dates, it errors", async () => {
-    initialState.epochStartBlock = null;
-    initialState.marketStartBlock = null;
-    expect(() => getStartAndEndBlock(initialState)).to.throw();
-  });
+    it("if there are no start dates, it errors", async () => {
+        initialState.epochStartBlock = null;
+        initialState.marketStartBlock = null;
+        expect(() => getStartAndEndBlock(initialState)).to.throw();
+    });
 
-  it("if there is no market start date, it errors", async () => {
-    initialState.marketStartBlock = null;
-    expect(() => getStartAndEndBlock(initialState)).to.throw();
-  });
+    it("if there is no market start date, it errors", async () => {
+        initialState.marketStartBlock = null;
+        expect(() => getStartAndEndBlock(initialState)).to.throw();
+    });
 
-  // ENDS
+    // ENDS
 
-  it("if the market ends before the epoch, end block is market block", async () => {
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.marketEndBlock);
-  });
+    it("if the market ends before the epoch, end block is market block", async () => {
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.marketEndBlock);
+    });
 
-  it("if the epoch ends before the market, end block is epoch block", async () => {
-    initialState.epochEndBlock = 21500000;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.epochEndBlock);
-  });
+    it("if the epoch ends before the market, end block is epoch block", async () => {
+        initialState.epochEndBlock = 21500000;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.epochEndBlock);
+    });
 
-  it("if the market ends and the epoch has not ended, end block is market block", async () => {
-    initialState.epochEndBlock = null;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.marketEndBlock);
-  });
+    it("if the market ends and the epoch has not ended, end block is market block", async () => {
+        initialState.epochEndBlock = null;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.marketEndBlock);
+    });
 
-  it("if the epoch ends and the market has not resolved, end block is epoch block", async () => {
-    initialState.marketEndBlock = null;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.epochEndBlock);
-  });
+    it("if the epoch ends and the market has not resolved, end block is epoch block", async () => {
+        initialState.marketEndBlock = null;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.epochEndBlock);
+    });
 
-  it("if the epoch has not ended and the market has not resolved, end block is null", async () => {
-    initialState.epochEndBlock = null;
-    initialState.marketEndBlock = null;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(null);
-  });
+    it("if the epoch has not ended and the market has not resolved, end block is null", async () => {
+        initialState.epochEndBlock = null;
+        initialState.marketEndBlock = null;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(null);
+    });
 
-  it("if the epoch ends and the market has not resolved, end block is epoch block", async () => {
-    initialState.marketEndBlock = null;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.epochEndBlock);
-  });
+    it("if the epoch ends and the market has not resolved, end block is epoch block", async () => {
+        initialState.marketEndBlock = null;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.epochEndBlock);
+    });
 
-  it("if there is a rewardMarketEndBlock, use it", async () => {
-    initialState.rewardMarketEndBlock = 21500000;
-    const { endBlock } = getStartAndEndBlock(initialState);
-    expect(endBlock).to.eq(initialState.rewardMarketEndBlock);
-  });
+    it("if there is a rewardMarketEndBlock, use it", async () => {
+        initialState.rewardMarketEndBlock = 21500000;
+        const { endBlock } = getStartAndEndBlock(initialState);
+        expect(endBlock).to.eq(initialState.rewardMarketEndBlock);
+    });
 });
